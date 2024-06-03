9a77c02cdc2da88c349fcdcf8279aec256a12505
Sakshi Bhalla <97746699+SakshiBhalla77@users.noreply.github.com>
2024-06-03 14:42:41 +0530

    Time: 44 ms (31.66%), Space: 17.1 MB (68.85%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,64 +8,61 @@
  */
 class Solution {
 public:
+    // APPROACH ONE
 
-//APPROACH ONE 
+    /*    int findLength(ListNode* head) {
+            int c = 0;
 
-/*    int findLength(ListNode* head) {
-        int c = 0;
-
-        while (head != NULL) {
-            c++;
-            head = head->next;
-        }
-        return c;
-    }
-    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        int a = findLength(headA);
-        int b = findLength(headB);
-
-        if (b > a) {
-            for (int i = 0; i < (b - a); i++)
-                headB = headB->next;
-        } else if (a > b) {
-            for (int i = 0; i < (a - b); i++)
-                headA = headA->next;
-        }
-
-        while (headA != NULL && headB != NULL) {
-            if (headA == headB) {
-                return headA;
+            while (head != NULL) {
+                c++;
+                head = head->next;
             }
-            headA = headA->next;
-            headB = headB->next;
+            return c;
         }
-        return NULL;
+        ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+            int a = findLength(headA);
+            int b = findLength(headB);
+
+            if (b > a) {
+                for (int i = 0; i < (b - a); i++)
+                    headB = headB->next;
+            } else if (a > b) {
+                for (int i = 0; i < (a - b); i++)
+                    headA = headA->next;
+            }
+
+            while (headA != NULL && headB != NULL) {
+                if (headA == headB) {
+                    return headA;
+                }
+                headA = headA->next;
+                headB = headB->next;
+            }
+            return NULL;
+        }
+    }; */
+
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+
+        while (temp1 != temp2) {
+            if (temp1 != NULL)
+                temp1 = temp1->next;
+
+            else {
+                temp1 = headB;
+            }
+
+            if (temp2 != NULL)
+                temp2 = temp2->next;
+
+            else 
+                temp2 = headA;
+            
+            // temp1 = temp1 == NULL? headB:temp1->next;
+            // temp2 = temp2 == NULL? headA:temp2->next;
+        }
+        return temp2; // return temp2 will work too
     }
-}; */
-
- ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    ListNode* temp1 = headA;
-    ListNode* temp2 = headB;
-
-    while(temp1 !=temp2){
-        if(temp1!=NULL)
-        temp1 = temp1->next;
-
-        else{
-            temp1= headB;
-        }
-
-        if(temp2!=NULL)
-        temp2 = temp2->next;
-
-        else{
-            temp2= headA;
-        }
-
-
-        // temp1 = temp1 == NULL? headB:temp1->next;
-        // temp2 = temp2 == NULL? headA:temp2->next;
-    }
-    return temp2; //return temp2 will work too
-}
 };
