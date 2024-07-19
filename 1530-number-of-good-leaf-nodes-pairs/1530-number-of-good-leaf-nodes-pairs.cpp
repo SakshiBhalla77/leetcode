2c10111987bc3f19f21d27f742f92a972b9b1c96
Sakshi Bhalla <97746699+SakshiBhalla77@users.noreply.github.com>
2024-07-19 17:22:12 +0530

    Time: 276 ms (6.5%), Space: 118.5 MB (5.07%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int countPairs(TreeNode* root, int distance) {
+        unordered_map<TreeNode*, vector<TreeNode*>> graph;
+        unordered_set<TreeNode*> leafNodes;
+
+        traverseTree(root, nullptr, graph, leafNodes);
+
+        int ans = 0;
+        for (TreeNode* leaf : leafNodes) {
+            queue<TreeNode*> bfsQueue;
+            unordered_set<TreeNode*> seen;
+            bfsQueue.push(leaf);
+            seen.insert(leaf);
+
+            for (int i = 0; i <= distance; i++) {
+                int size = bfsQueue.size();
+                for (int j = 0; j < size; j++) {
+                    TreeNode* currNode = bfsQueue.front();
+                    bfsQueue.pop();
+                    if (leafNodes.count(currNode) && currNode != leaf) {
+                        ans++;
+                    }
+                    if (graph.count(currNode)) {
+                        for (TreeNode* neighbor : graph[currNode]) {
+                            if (!seen.count(neighbor)) {
+                                bfsQueue.push(neighbor);
+                                seen.insert(neighbor);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ans / 2;
+    }
+
+private:
+    void traverseTree(TreeNode* currNode, TreeNode* prevNode, 
+                      unordered_map<TreeNode*, vector<TreeNode*>>& graph, 
+                      unordered_set<TreeNode*>& leafNodes) {
+        if (!currNode) return;
+        if (!currNode->left && !currNode->right) {
+            leafNodes.insert(currNode);
+        }
+        if (prevNode) {
+            graph[prevNode].push_back(currNode);
+            graph[currNode].push_back(prevNode);
+        }
+        traverseTree(currNode->left, currNode, graph, leafNodes);
+        traverseTree(currNode->right, currNode, graph, leafNodes);
+    }
+};
