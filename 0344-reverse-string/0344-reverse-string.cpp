08d0a6318ad2bea9e25494d3d887de71f7d49875
Sakshi Bhalla <97746699+SakshiBhalla77@users.noreply.github.com>
2024-10-05 21:25:23 +0530

    Time: 25 ms (7.85%), Space: 28.1 MB (5.68%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
+/*
+// TIME = O(N) SPACE O(1) - iterative solution
     void reverseString(vector<char>& s) {
         int n = s.size();
         int i =0; int j = n-1;
        
         while(i<=j){
             swap(s[i],s[j]);
-
             i = i+1; 
             j = j-1;
         }
         
         
     }
+    */
+
+    // STACK USE KRKE
+void reverseString(vector<char>& s) 
+{
+    stack<char>st;
+    for(int i =0; i<s.size();i++)
+    {
+        st.push(s[i]);
+    }
+    for(int i =0; i<s.size();i++)
+    {
+        s[i] = st.top();
+        st.pop();
+        
+    }
+}
+
 };
