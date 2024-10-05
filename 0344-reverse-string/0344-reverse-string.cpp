8b23316f44af489db1d6bb402f50b10671cd8bb0
Sakshi Bhalla <97746699+SakshiBhalla77@users.noreply.github.com>
2024-10-05 21:44:04 +0530

    Time: 19 ms (46.96%), Space: 27.1 MB (86.37%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -16,6 +16,9 @@ public:
     }
     */
 
+
+
+/*
     // STACK USE KRKE
     // time O(N) SPACE O(N)
 void reverseString(vector<char>& s) 
@@ -32,5 +35,24 @@ void reverseString(vector<char>& s)
         
     }
 }
+*/
 
+
+
+//RECURSIVE APPROACH 
+
+void reverseString(vector<char>& s) 
+{
+    int n = s.size();
+        reverse(s,0,n-1);
+    
+    
+}
+void reverse(vector<char>& s, int i , int j)
+{
+   if(i<j){
+    swap(s[i],s[j]);
+    reverse(s, i + 1, j - 1);
+   }
+}
 };
